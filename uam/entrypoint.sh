6f38a09dc083042ebb2c8ab8fb2ec04fd5bd336c
Tha14 <Tha14@users.noreply.github.com>
2022-12-13 19:23:16 +0200

    Replace host networking with static port forwarding. Attempt to fix DNS DoH resolution.

--- uam/entrypoint.sh
+++ uam/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /tmp
-apt update && apt -yq install wget libglib2.0-0 ca-certificates
+apt update && apt -yq install wget libglib2.0-0 ca-certificates dnsmasq
 update-ca-certificates
 wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
 dpkg -i uam-latest_amd64.deb
 cd /opt/uam/
-./uam --pk $1 --http [127.0.0.1]:17099 --no-ui
+./uam --pk $1 --http [0.0.0.0]:17099 --no-ui
