8c43a023c3e6ea7346d537bd47722da557dde437
Tha14 <Tha14@users.noreply.github.com>
2023-01-06 17:47:40 +0200

    fix: enable bandwidth shaping after downloading uam

--- uam-swarm/entrypoint.sh
+++ uam-swarm/entrypoint.sh
@@ -2,11 +2,11 @@
 cd /tmp
 apt update && apt -yq install wget libglib2.0-0 ca-certificates dnsmasq wondershaper
 update-ca-certificates
-wondershaper eth0 $3 $4 &
 wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
 dpkg -i uam-latest_amd64.deb
 cd /opt/uam/
 echo "[net]" >> /root/.uam/uam.ini
 container_ip="$(hostname -i)"
 echo "listens=[${container_ip}]:$2" >> /root/.uam/uam.ini
+wondershaper eth0 $3 $4 &
 ./uam --pk $1 --http [0.0.0.0]:17099 --no-ui
--- uam/entrypoint.sh
+++ uam/entrypoint.sh
@@ -2,11 +2,11 @@
 cd /tmp
 apt update && apt -yq install wget libglib2.0-0 ca-certificates dnsmasq wondershaper
 update-ca-certificates
-wondershaper eth0 $2 $3 &
 wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
 dpkg -i uam-latest_amd64.deb
 cd /opt/uam/
 echo "[net]" >> /root/.uam/uam.ini
 container_ip="$(hostname -i)"
 echo "listens=[${container_ip}]:4156" >> /root/.uam/uam.ini
+wondershaper eth0 $2 $3 &
 ./uam --pk $1 --http [0.0.0.0]:17099 --no-ui
