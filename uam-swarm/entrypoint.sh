4e2a83e4cabc7fa688c5a7894cd88b73fd19192d
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-01-10 16:46:09 +0700

    Update entrypoint.sh

--- uam-swarm/entrypoint.sh
+++ uam-swarm/entrypoint.sh
@@ -8,5 +8,5 @@ cd /opt/uam/
 echo "[net]" >> /root/.uam/uam.ini
 container_ip="$(hostname -i)"
 echo "listens=[${container_ip}]:$2" >> /root/.uam/uam.ini
-# wondershaper ens3 $3 $4 &
+# wondershaper eth0 $3 $4 &
 ./uam --pk $1 --http [0.0.0.0]:17099 --no-ui
