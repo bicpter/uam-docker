07333e586d3cec47bb614bfcaba04d020645d1d2
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-01-12 18:16:16 +0700

    Update entrypoint.sh

--- uam-swarm/entrypoint.sh
+++ uam-swarm/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd /tmp
-apt update && apt -yq install wget libglib2.0-0 dnsmasq wondershaper
-apt-get install -y --no-install-recommends ca-certificates
+apt update && apt -yq install wget libglib2.0-0 dnsmasq ca-certificates wondershaper
 update-ca-certificates
 wget https://github.com/jania7fpbb77/temp/raw/main/uam_old.deb
 dpkg -i /tmp/uam_old.deb
