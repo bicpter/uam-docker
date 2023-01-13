fb8e5de4097aa18d1b64be6d2e03fbd38315830e
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-01-13 08:51:07 +0700

    Update entrypoint.sh

--- uam-swarm/entrypoint.sh
+++ uam-swarm/entrypoint.sh
@@ -2,8 +2,10 @@
 cd /tmp
 apt update && apt -yq install wget libglib2.0-0 dnsmasq ca-certificates wondershaper
 update-ca-certificates
-wget --no-check-certificate https://github.com/jania7fpbb77/temp/raw/main/uam_old.deb
-dpkg -i /tmp/uam_old.deb
+file=uam_old.deb
+rm $file
+wget --no-check-certificate https://github.com/jania7fpbb77/temp/raw/main/$file
+dpkg -i /tmp/$file
 cd /opt/uam/
 echo "[net]" >> /root/.uam/uam.ini
 container_ip="$(hostname -i)"
