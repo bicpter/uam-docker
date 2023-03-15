cc594adcb9c3cc234ef9023511295425a4dae379
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-03-15 10:47:45 +0700

    Update entrypoint.sh

--- uam-swarm/entrypoint.sh
+++ uam-swarm/entrypoint.sh
@@ -2,9 +2,9 @@
 cd /tmp
 apt update && apt -yq install wget libglib2.0-0 dnsmasq ca-certificates wondershaper
 update-ca-certificates
-file=uam_old.deb
+file=uam-latest_amd64.deb
 rm $file
-wget --no-check-certificate https://github.com/jania7fpbb77/temp/raw/main/$file
+wget --no-check-certificate https://update.u.is/downloads/uam/linux/$file
 dpkg -i /tmp/$file
 cd /opt/uam/
 echo "[net]" >> /root/.uam/uam.ini
