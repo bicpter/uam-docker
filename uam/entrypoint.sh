7762c632aa6dbadecec610946921584f12e3ffa6
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-04-12 21:43:17 +0700

    Update entrypoint.sh

--- uam/entrypoint.sh
+++ uam/entrypoint.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 cd /tmp
-apt update && apt -yq install wget libglib2.0-0 ca-certificates dnsmasq wondershaper
+apt update && apt -yq install wget libglib2.0-0 dnsmasq ca-certificates wondershaper
 update-ca-certificates
-wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
-dpkg -i uam-latest_amd64.deb
+file=uam-latest_amd64.deb
+rm $file
+wget --no-check-certificate https://update.u.is/downloads/uam/linux/$file
+dpkg -i /tmp/$file
 cd /opt/uam/
 echo "[net]" >> /root/.uam/uam.ini
 container_ip="$(hostname -i)"
-echo "listens=[${container_ip}]:4156" >> /root/.uam/uam.ini
-wondershaper eth0 $2 $3 &
+echo "listens=[${container_ip}]:$2" >> /root/.uam/uam.ini
+# wondershaper eth0 $3 $4 &
 ./uam --pk $1 --http [0.0.0.0]:17099 --no-ui
